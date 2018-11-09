e3a94f7f79fde29d12686435f1f584ca74be457d
yeonsuuuu <yeonsoo39@naver.com>
2018-11-09 10:20:20 +0900

    Read 기능 구현

---
+++ express-basic/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+const movies = [
+  { id: 1, title: "미쓰백" },
+  { id: 2, title: "HarryPorter" },
+  { id: 3, title: "Ant Man" }
+];
+
+app.get("/", (req, res) => {
+  res.send("Happy Hacking");
+});
+
+app.get("/:name", (req, res) => {
+  res.send(`Hi, ${req.params.name}`);
+});
+
+// CRUD
+// CREATE READ UPDATE DESTORY
+// POST GET PUT DELETE
+
+/* GET /api/movies */
+app.get("/api/movies", (req, res) => {
+  res.send(movies);
+});
+
+/* GET /api/movies/1 */
+app.get('/api/movies/:id', (req, res) => {
+  const movie = movies.find((movie) => {
+    return movie.id === parseInt(req.params.id);
+  });
+  if(!movie) {
+    res.status(404).send(`Movie with given id(${req.params.id}) is not found`);
+  }
+  console.log(movie);
+  res.send(movie);
+});
+
+// /* POST /api/movies/1 */
+// app.post();
+
+// /* PUT /api/movies/1 */
+// app.put();
+
+// /* DELETE /api/movies/1 */
+// app.delete();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
