6e3cb7fec9111cc1d6e9d93137c3270039893a2a
yeonsuuuu <yeonsoo39@naver.com>
2018-11-13 15:29:55 +0900

    mongodb Query Finish

--- MongoDB-basic/index.js
+++ MongoDB-basic/index.js
@@ -1,44 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/hello-mongo',{ useNewUrlParser: true})
-  .then(() => console.log('Connencted to MongoDB'))
+mongoose
+  .connect(
+    "mongodb://localhost/hello-mongo",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connencted to MongoDB"))
   .catch(error => console.error(error.message));
 
-  //Available Schema Datatypes : String, Number, Date, Buffer, Boolean, ObjectID, Array
+//Available Schema Datatypes : String, Number, Date, Buffer, Boolean, ObjectID, Array
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
-  tags: [ String ],
-  date: { type: Date, default: Date.now},
+  tags: [String],
+  date: { type: Date, default: Date.now },
   isPublished: Boolean
-})
+});
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 /* CRUD Operation */
 
 async function createCourese() {
   const course = new Course({
-    name: '실전 DApp 빌드',
-    author: 'john',
-    tags: ['Ethereum', 'Blockchain', 'DAapp'],
+    name: "실전 DApp 빌드",
+    author: "john",
+    tags: ["Ethereum", "Blockchain", "DAapp"],
     isPublished: false
   });
-  try{
+  try {
     const result = await course.save();
-    console.log(result)
-  }catch(error) {
+    console.log(result);
+  } catch (error) {
     console.error(error.message);
   }
 }
 
 async function getCourses() {
   const courses = await Course
-  //.find({ price: {  $gt: 15}})
-    .find({ isPublished: true}) // find.(); 전체를 가져오는 것 
-    .limit(10)
-    .sort({ name: -1})
-    .select({ name: 1, tags: 1})
+    //.find({ price: {  $lt: 15, $gt: 15}})
+    //.fing({ price: { $in: [10, 15] } })
+    // .find({ isPublished: true}) // find.(); 전체를 가져오는 것
+    // .limit(10)
+    // .sort({ name: -1})
+    // .select({ name: 1, tags: 1})
+    // .find({ author: /^su/ }) //author 중에 su로 시작하는 것
+    // .find({ author: /hn$/ }) //끝에 문자열이 hn인 것
+    // .find({ author: /.*oh.*/ }); //앞뒤 상관없이 oh가 있는 모든 문자열
+    // .count()
   console.log(courses);
 }
 
@@ -53,5 +62,13 @@ async function getCourses() {
   $nin
 */
 
+/* 논리 쿼리 연산자
+  .or
+    Course.find()
+      .or([{ author: 'suuuu'}, { isPublished: false }])
+  .and
+    Cousrse.find()
+      .and([{ author: 'suuuu'}, { isPublished: false }])
+*/
 
 getCourses();
